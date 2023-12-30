c522ebe68819dd122f46a6622d1fd5eaf6a0e1b8
Naveen55066 <naveen.r@brigosha.com>
2023-12-30 23:10:32 +0530

    naveen changes

---
+++ Portfolio/src/data/user.js
@@ -0,0 +1,67 @@
+import reactIcon from "../Assests/react icon.svg"
+
+const INFO = {
+	main: {
+		title: "Reactfolio by Naveen",
+		name: "Naveen R.",
+		email: "naveenramdas007@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Frontend web developer dedicated to designing responsive and user-friendly interfaces for web applications.",
+		description:
+			"I am a frontend web developer with expertise in React.js. I have experience in building responsive, user-friendly interfaces for web applications, ensuring scalability, security, and reliability. Proficient in utilizing various frameworks and technologies, I enjoy solving intricate problems and continuously expanding my skill set. My passion lies in crafting high-quality code that adheres to best practices and industry standards. I am enthusiastic about taking on new challenges and seizing opportunities to further develop as a React.js developer."
+	},
+
+	about: {
+		title: "I’m Naveen R. I live in India, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "DTH Recharge Web Application",
+			description:
+				"Users may choose DTH operators, review plans, and rapidly reload using this recharge application. They can connect with their registered email address or mobile number.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/NaveenRamdas/DTH-Recharge-System",
+		},
+
+		{
+			title: "TODO LIST",
+			description:
+				"Users can add new tasks to the list. this typically involves providing a task title and, optionally, a description or due date. they can perform CRUD operations.",
+			logo: reactIcon,
+			linkText: "View Project",
+			link: "https://github.com/NaveenRamdas/React-Projects",
+		},
+
+		{
+			title: "E-Commerce Shopping Page",
+			description:
+				"Here, clients are able to shop. They have the option to check out and pay after adding different items to their cart.",
+			logo: reactIcon,
+			linkText: "View Project",
+			link: "https://github.com/NaveenRamdas/ECOMMERCE.APP",
+		},
+	],
+};
+
+export default INFO;
